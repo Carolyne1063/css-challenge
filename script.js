23f2fd52ca23dc067ddec3351ec3996ddd7cf87c
Carolyne1063 <carolmartoz763@gmail.com>
2024-05-13 19:20:44 -0700

    css challenge

---
+++ script.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+const Book_Appointment = document.querySelector('.book-appointment');
+
+
+
+Book_Appointment.addEventListener('click', ()=>{wrapper.classList.add('book-appointment');});
+
